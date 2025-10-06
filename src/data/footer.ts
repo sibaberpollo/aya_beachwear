76e9902524c388859df13ccc3ff3b2d0706eedb9
José Pino <pino.jose@gmail.com>
2025-10-06 15:22:41 -0300

    solo html

--- src/data/footer.ts
+++
@@ -1,9 +0,0 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
-
-// Leer el HTML en tiempo de compilación (build time)
-export const footerHtml = readFileSync(
-  join(process.cwd(), 'src', 'data', 'footer.html'),
-  'utf-8'
-);
-
--- src/data/header.ts
+++
@@ -1,9 +0,0 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
-
-// Leer el HTML en tiempo de compilación (build time)
-export const headerHtml = readFileSync(
-  join(process.cwd(), 'src', 'data', 'header.html'),
-  'utf-8'
-);
-
